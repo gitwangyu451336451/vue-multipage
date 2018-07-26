55dac3676ba593234ee074cbcfdb323e0cd50d51
wangyu <451336451@qq.com>
2018-07-26 12:39:03 +0800

    测试

---
+++ src/pages/page1/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <a v-for="item in data" :key="item">{{item+2}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      data: [1, 2, 3]
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+</style>
